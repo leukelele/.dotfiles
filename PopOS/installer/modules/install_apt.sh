d0cc91fa0c59ce9d85bf7ecc4619eee1f7cc7b34
Luke Le <70491331+Ricealicious@users.noreply.github.com>
2021-09-11 01:56:46 -0500

    install codium

--- PopOS/installer/modules/install_apt.sh
+++ PopOS/installer/modules/install_apt.sh
@@ -23,9 +23,14 @@ install timeshift
 # development
 install git
 install vim
-install codium
 
 # applications
 install thunderbird
 install firefox
 install discord
+
+# exception cases
+# installs codium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt install codium

efb31684935baebf76fc59779b9e7813b29f90c8
Lukelele <70491331+Ricealicious@users.noreply.github.com>
2021-09-11 02:30:31 -0500

    removes unwanted apps

--- PopOS/installer/installer.sh
+++ PopOS/installer/installer.sh
@@ -3,9 +3,11 @@
 cd modules
 ./install_apt.sh
 ./install_flatpak.sh
+./remove.sh
 
-# upgrades all
+# upgrades and clean up
 sudo apt upgrade -y
+sudo apt autoremove
 
 # see bash changes
 source ~/.bashrc
---
+++ PopOS/installer/modules/remove.sh
@@ -0,0 +1,4 @@
+!/bin/bash
+
+rm -y gedit
+rm -y evince

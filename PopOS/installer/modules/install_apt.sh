aadb52d0926f69e165c0768d0f925941a7b62ffd
Lukelele <70491331+Ricealicious@users.noreply.github.com>
2021-09-11 02:08:09 -0500

    small changes

---
+++ PopOS/installer/modules/install_apt.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+function install
+{
+    which $1 &> /dev/null
+
+    if [ $? -ne 0 ]; then
+        echo "Installing: ${1}..."
+        sudo apt install -y $1
+    else
+        echo "Already installed: ${1}"
+    fi
+}
+
+# system
+install gnome-tweaks
+install gnome-shell-extensions
+install timeshift
+
+# development
+install git
+install vim
+g++
+
+# applications
+install thunderbird
+install firefox
+install discord
+
+# exception cases
+# installs codium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt install codium

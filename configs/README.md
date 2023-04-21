c6d590b2b134104b5e1c21b887a0b3bba1c74b66
Lukelele <luke.bk.le@gmail.com>
2023-04-21 19:09:50 -0400

    installation script for an arch/arch-based system

--- configs/README.md
+++ configs/README.md
@@ -1,3 +1,1 @@
-This folder is organized such that it would reflect how it would appear in the home directory. 
-- The files outside of the `.config` folder will be placed directly in the home directory.
-- The files in this `.config` folder will be placed in the system's `.config` foler.
+This folder is organized such that it would reflect how it would mostly appear in the home directory.
--- install.sh
+++ install.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+# install necessary apps
+sudo pacman -Syu
+sudo pacman -S discord keepassxc neovim zathura zathura-pdf-mupdf flatpak pdfarranger xournalpp
 
-ln -s $HOME/.dotfiles/configs/.gitconfig $HOME/
+flatpak update -y
+flatpak install -y app/md.obsidian.Obsidian/x86_64/stable
+
+yay -S timeshift protonvpn
+
+# symlinks
+create_config_link() {
+    # Check if the argument is provided
+    if [ $# -eq 0 ]; then
+        echo "Usage: create_config_link <link-name>"
+        return 1
+    fi
+
+    # Create the symlink to the .config directory
+    ln -sf $HOME/.dotfiles/configs/.config/$ARG $HOME/.config/$ARG
+
+    echo "Symbolic links created successfully!"
+}
+
+create_config_link ../.gitconfig
+create_config_link zathura
+create_config_link nvim

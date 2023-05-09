c0b2c727d0d502ea2fb93a98cdd0cc4f46694abf
Lukelele <luke.bk.le@gmail.com>
2023-05-09 01:39:10 -0500

    change to file name and installation script

--- install.sh
+++ install.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-# install necessary apps
+# install work apps
 sudo pacman -Syu
 sudo pacman -S discord keepassxc neovim zathura zathura-pdf-mupdf flatpak pdfarranger xournalpp
 
 flatpak update -y
 flatpak install -y app/md.obsidian.Obsidian/x86_64/stable
 
-yay -S timeshift protonvpn
+yay -S timeshift protonvpn spotify
+
+# remove unnecessary apps
+sudo pacman -Rns nano
 
 # symlinks
 create_config_link() {
-    # Check if the argument is provided
-    if [ $# -eq 0 ]; then
-        echo "Usage: create_config_link <link-name>"
-        return 1
-    fi
-
     # Create the symlink to the .config directory
     ln -sf $HOME/.dotfiles/configs/.config/$ARG $HOME/.config/$ARG
 
     echo "Symbolic links created successfully!"
 }
 
+create_config_link ../.vimrc
 create_config_link ../.gitconfig
 create_config_link zathura
 create_config_link nvim

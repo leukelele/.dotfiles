e2313f73325bb3b3fa9ef53cf033f1cb53112e38
leukelele <luke.bk.le@gmail.com>
2023-06-03 15:51:45 -0500

    moved directories for ease of navigation

--- install.sh
+++ install.sh
@@ -1,23 +1,57 @@
 #!/bin/bash
-# install work apps
-sudo pacman -Syu
-sudo pacman -S discord keepassxc neovim zathura zathura-pdf-mupdf flatpak pdfarranger xournalpp
 
+#
+# Get the directory paths used by the bash script
+#
+script_dir=$(dirname "$(readlink -f "${0:A}")")
+dotconfig_dir=script_dir/configs/.config
+
+#
+# given two directories, creates a symlink of the original directory in dest. directory
+#
+create_symlink() {
+    original_dir=$1
+    symlink_dir=$2
+
+    if [ $# -eq 0 ]; then
+        original_dir="$dotconfig_dir"/*
+        symlink_dir="$HOME/.config"
+
+    elif [ $# -eq 1 ]; then
+        original_dir="$script_dir/$1"
+        symlink_dir="$HOME/.config"
+
+    else 
+        original_dir="$script_dir/$1"
+        symlink_dir="$2"
+    fi
+
+    ln -sf "$original_dir" "$symlink_dir"
+}
+
+#
+# install apps and necessities
+#
+
+# pacman wrapper for convenience
+sudo pacman -Syu
+sudo pacman -S yay
+
+# apps
+yay -S discord keepassxc neovim zathura zathura-pdf-mupdf pdfarranger spotify xournalpp 
+
+# app distribution; mainly used for access towards obsidian
+yay -S flatpak
 flatpak update -y
 flatpak install -y app/md.obsidian.Obsidian/x86_64/stable
 
-yay -S timeshift protonvpn spotify
+#
+# install dotfile dependencies and create necessary symlinks
+#
+yay -S mako swaylock-effects wofi waybar-hyprland-git timeshift protonvpn nwg-look nemo xfce4-terminal kitty
 
+#
 # remove unnecessary apps
+#
 sudo pacman -Rns nano
 rm .nanorc
-
-# symlinks
-create_config_link() {
-    # Create the symlink to the .config directory
-    local ARG=$1
-    ln -sf $HOME/.dotfiles/configs/.config/$ARG $HOME/.config/
-    echo "Symbolic link created successfully!"
-}
-
-create_config_link *

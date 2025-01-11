131ef61d5c1b5b5c2a5383b293ef599a8aa091e5
leukelele <70491331+leukelele@users.noreply.github.com>
2025-01-10 23:06:32 -0600

    revamp bc new not struct

--- qtile/scripts/random.sh
+++ qtile/scripts/random.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-dir=$HOME"/"
-dir+=".nb/"
-dir+=$(ls $dir | shuf -n 1)"/"
-dir+=$(ls $dir | shuf -n 1)
-vim $dir
+
+# define the base directory path
+base_dir="$HOME/.nb/"
+
+# randomly select between the "fleeting" and "permanent" directories
+selected_dir=$(shuf -e "fleeting" "permanent" -n 1)
+
+# get a random file from the selected directory
+file=$(shuf -e $(find "$base_dir$selected_dir" -type f) -n 1)
+
+# open the selected file with vim
+vim "$file"

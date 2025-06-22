14eed429b4c8ed2561da3d8c54101f56869017fd
leukelele <70491331+leukelele@users.noreply.github.com>
2025-06-21 22:00:19 -0500

    plugin config updated

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,36 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  lazy = true,
-  dependencies = { 'nvim-lua/plenary.nvim', 'Burntsushi/ripgrep' },
-  keys = {
-    { "<leader>dv", mode = "n", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>dc", mode = "n", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-  },
+	'nvim-telescope/telescope.nvim', 
+	tag = '0.1.8',
+	lazy = true,
+
+	dependencies = { 'nvim-lua/plenary.nvim', 'Burntsushi/ripgrep' },
+
+	keys = {
+		{ "<leader>dv", mode = "n", "<cmd>Telescope find_files<cr>", 
+		  desc = "Find Files" },
+		{ "<leader>dc", mode = "n", "<cmd>Telescope live_grep<cr>", 
+		  desc = "Live grep" },
+	},
+
+	opts = {
+		defaults = {
+			vimgrep_arguments = {
+				-- preserve ripgrep's defaults but include hidden files
+				unpack(require('telescope.config').values.vimgrep_arguments),
+				"--hidden",
+				"--glob", "!**/.git/*",
+			},
+
+			mappings = {
+				i = { ["<esc>"] = require("telescope.actions").close },
+			},
+		},
+
+		pickers = {
+			find_files = {
+				find_command = { "rg", "--files", "--hidden", "--glob", 
+				"!**/.git/*" },
+			},
+		},
+	},
 }

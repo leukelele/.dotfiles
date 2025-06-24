557e5b7725b0e2ebbe62a5144277dba6d63e8326
leukelele <70491331+leukelele@users.noreply.github.com>
2025-06-24 01:32:20 -0500

    convert spaces to tabs

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,14 +1,14 @@
 -- bootstrap lazy.nvim (git clone if repo not present)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,27 +17,27 @@ require('config.options')
 require('config.keymaps')
 
 local opts = {
-  defaults = {
-    lazy = true,
-  },
-  install = {
-    colorscheme = { "catppuccin" }
-  },
-  rtp = {
-    disabled_plugins = {
-      "gzip",
-      "matchit",
-      "matchparen",
-      "netrwPlugin",
-      "tarPlugin",
-      "tohtml",
-      "tutor",
-      "zipPlugin",
-    },
-  },
-  change_detection = {
-    notify = false,
-  }
+	defaults = {
+	  lazy = true,
+	},
+	install = {
+		colorscheme = { "catppuccin" }
+	},
+	rtp = {
+		disabled_plugins = {
+			"gzip",
+			"matchit",
+			"matchparen",
+			"netrwPlugin",
+			"tarPlugin",
+			"tohtml",
+			"tutor",
+			"zipPlugin",
+		},
+	},
+	change_detection = {
+		notify = false,
+	}
 }
 
 require("lazy").setup('plugins', opts)

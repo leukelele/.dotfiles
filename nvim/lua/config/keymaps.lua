55ac7383d2666b69640f795b08b3fe45e68ba268
leukelele <70491331+leukelele@users.noreply.github.com>
2025-06-24 01:31:38 -0500

    add function to jump to footnote and moved vim keymaps to this file

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,12 @@ local opts = { noremap = true, silent = true }
 -- directory navigation
 map("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
 
+-- goto footnote
+map("n", "gD", function()
+	local word = vim.fn.expand("<cword>") -- gets ^1 when cursor is on [^1]
+	vim.cmd("/\\[\\^" .. word:gsub("%^", "") .. "\\]:")
+end, { desc = "Jump to footnote definition" })
+
 -- pane and window navigation
 map("n", "<C-h>", "<C-w>h", opts)
 map("n", "<C-j>", "<C-w>j", opts)
@@ -13,6 +19,10 @@ map("n", "<C-h>", ":TmuxNavigateLeft<CR>", opts)
 map("n", "<C-j>", ":TmuxNavigateDown<CR>", opts)
 map("n", "<C-k>", ":TmuxNavigateUp<CR>", opts)
 map("n", "<C-l>", ":TmuxNavigateRight<CR>", opts)
+
+-- vim navigation
+map("i", "kj", "<Esc>")
+map("i", "KJ", "<Esc>")
 
 -- window management
 map("n", "<leader>sv", ":vsplit<CR>", opts)

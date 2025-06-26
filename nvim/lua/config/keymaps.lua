394fed002eea140a77ba62fde903d568ba1c5349
leukelele <70491331+leukelele@users.noreply.github.com>
2025-06-25 19:53:21 -0500

    added lsp action, telescope, and removed the goto function

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,16 +1,12 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- directory navigation
-map("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
+-- goto
+map("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
 
--- goto footnote
-map("n", "gD", function()
-	local word = vim.fn.expand("<cword>") -- gets ^1 when cursor is on [^1]
-	vim.cmd("/\\[\\^" .. word:gsub("%^", "") .. "\\]:")
-end, { desc = "Jump to footnote definition" })
-
-map("n", "gd", vim.lsp.buf.definition, { desc = "LSP: goto definition" })
+-- lsp behaviors
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+map("i", "<A-CR>", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
 
 -- pane and window navigation
 map("n", "<C-h>", "<C-w>h", opts)
@@ -22,10 +18,22 @@ map("n", "<C-j>", ":TmuxNavigateDown<CR>", opts)
 map("n", "<C-k>", ":TmuxNavigateUp<CR>", opts)
 map("n", "<C-l>", ":TmuxNavigateRight<CR>", opts)
 
+-- telescope
+map("n", "<leader>jf", function()
+	require("telescope.builtin").find_files()
+end, { desc = "Find Files" })
+
+map("n", "<leader>jd", function()
+	require("telescope.builtin").live_grep()
+end, { desc = "Live Grep" })
+
+-- tree
+map("n", "<leader>f", ":NvimTreeToggle<CR>", { desc = "Toggle Tree" })
+
 -- vim navigation
 map("i", "kj", "<Esc>")
 map("i", "KJ", "<Esc>")
 
 -- window management
-map("n", "<leader>sv", ":vsplit<CR>", opts)
-map("n", "<leader>sh", ":split<CR>", opts)
+map("n", "<leader>sv", ":vsplit<CR>")
+map("n", "<leader>sh", ":split<CR>")

d7779a3666121e274de554be98bb7757be15f77c
leukelele <70491331+leukelele@users.noreply.github.com>
2025-06-25 20:23:04 -0500

    removed unneded functions and moved keymappings to relevant file

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,33 +1,13 @@
 return {
-	'nvim-telescope/telescope.nvim', 
+	'nvim-telescope/telescope.nvim',
 	branch = '0.1.x',
 	lazy = true,
-	cmd = {"Telescope"},
-	dependencies = { 'nvim-lua/plenary.nvim', 'Burntsushi/ripgrep' },
-
-	keys = {
-		{ "<leader>dv", mode = "n", "<cmd>Telescope find_files<cr>", 
-		  desc = "Find Files" },
-		{ "<leader>dc", mode = "n", "<cmd>Telescope live_grep<cr>", 
-		  desc = "Live grep" },
-	},
-
+	cmd = { "Telescope" },
+	dependencies = { 'nvim-lua/plenary.nvim', 'burntsushi/ripgrep' },
 	opts = {
 		defaults = {
-			vimgrep_arguments = {
-				-- preserve ripgrep's defaults but include hidden files
-				unpack(require('telescope.config').values.vimgrep_arguments),
-				"--hidden",
-				"--glob", "!**/.git/*",
-			},
 			mappings = {
 				i = { ["<esc>"] = require("telescope.actions").close },
-			},
-		},
-		pickers = {
-			find_files = {
-				find_command = { "rg", "--files", "--hidden", "--glob", 
-				"!**/.git/*" },
 			},
 		},
 	},
